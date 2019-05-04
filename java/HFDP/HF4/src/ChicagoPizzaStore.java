81dffd7bbaa5b85bcc68a10aa0945c75c3001b2b
cymjr <cosmicy@outlook.com>
2019-05-04 15:52:38 +0800

    HF4添加Pizza预定的工厂模式

---
+++ java/HFDP/HF4/src/ChicagoPizzaStore.java
@@ -0,0 +1,8 @@
+public class ChicagoPizzaStore extends PizzaStore {
+    @Override
+    protected Pizza createPizza(String type) {
+        if (type.equals("cheese")) {
+            return new ChicagoStyleCheesePizza();
+        } else return null;
+    }
+}
---
+++ java/HFDP/HF4/src/NYPizzaStore.java
@@ -0,0 +1,9 @@
+public class NYPizzaStore extends PizzaStore {
+
+    @Override
+    protected Pizza createPizza(String type) {
+        if (type.equals("cheese")){
+            return new NYStyleCheesePizza();
+        } else return null;
+    }
+}
