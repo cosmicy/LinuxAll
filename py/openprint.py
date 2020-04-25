a98123a801414f55c0ebd74e1c54504e7d870dc5
cyub2004 <cosmicy@outlook.com>
2020-04-25 10:34:03 +0800

    python的rstrip

--- py/openprint.py
+++ py/openprint.py
@@ -2,4 +2,4 @@ file_name = 'fib.py'
 def openpy(filename):
     with open(filename) as file_object:
         for line in file_object:
-            print(line.rstrip())
+            print(line.rstrip()) #删除string字符串末尾的指定字符(默认为空格)
