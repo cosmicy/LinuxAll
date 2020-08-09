37955edbb3e3508cb16317622b0330502fad75f9
cy <cosmicy@outlook.com>
2020-08-09 10:38:57 +0800

    密码框隐藏字符

--- html/Vue/cli/ele-comp/src/components/accLogin/AccLogin.vue
+++ html/Vue/cli/ele-comp/src/components/accLogin/AccLogin.vue
@@ -1,18 +1,18 @@
 <template>
   <div>
-    <el-form :model="ruleForm" :rules="rules">
+    <el-form :model="ruleForm" :rules="rules" ref="ruleForm">
       <el-form-item prop="username">
         <el-input placeholder="user" v-model="ruleForm.username" @input="update($event)">
           <i slot="prefix" class="el-icon-user"></i>
         </el-input>
       </el-form-item>
       <el-form-item prop="password">
-        <el-input placeholder="password" v-model="ruleForm.password">
+        <el-input placeholder="password" v-model="ruleForm.password" show-password>
           <i slot="prefix" class="el-icon-lock"></i>
         </el-input>
       </el-form-item>
       <el-form-item>
-        <el-button type="primary" style="width: 100%">登录</el-button>
+        <el-button type="primary" style="width: 100%" @click="login">登录</el-button>
       </el-form-item>
     </el-form>
   </div>
