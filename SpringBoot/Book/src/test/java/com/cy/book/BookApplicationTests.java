05d351b461f9c37a3402c81fc37b7a2b07a9a940
cy <cosmicy@outlook.com>
2020-08-10 23:25:26 +0800

    添加SpringBoot项目Book图书查询

---
+++ SpringBoot/Book/src/test/java/com/cy/book/BookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cy.book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
